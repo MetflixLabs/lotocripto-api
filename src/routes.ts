8d90a5e0faf532177408f6b1650189f0d94521ff
eber404 <bodacamargo@gmail.com>
2021-02-13 14:35:31 -0300

    add: ask for auth in routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
-import aaa from './middlewares/auth'
+import { jwtAuth } from './middlewares/jwtAuth'
+import cookieParser from 'cookie-parser'
 import { loginController } from './useCases/Access/LoginUseCase'
 import { createParticipantController } from './useCases/Participant/CreateParticipantUseCase'
 import { listParticipantController } from './useCases/Participant/ListParticipantUseCase'
@@ -10,34 +11,36 @@ import { findUserByIdController } from './useCases/User/FindUserByIdUseCase'
 import { findUserByNameController } from './useCases/User/FindUserByNameUseCase'
 import { listUserController } from './useCases/User/ListUserUseCase'
 import { updateUserController } from './useCases/User/UpdateUserUseCase'
+
 const router = Router()
 
 // Login
-router.post('/login', (request, response) => loginController.handle(request, response))
+router.post('/login', (request, response, next) => loginController.handle(request, response, next))
 
-// Auth
-router.use(aaa)
+router.use(cookieParser())
 
 // users
-router.get('/users', (request, response) => {
+router.get('/users', jwtAuth, (request, response) => {
   if (request.body.id) return findUserByIdController.handle(request, response)
   else if (request.body.name) return findUserByNameController.handle(request, response)
 
   return listUserController.handle(request, response)
 })
 
-router.post('/users', (request, response) => createUserController.handle(request, response))
-router.put('/users', (request, response) => updateUserController.handle(request, response))
+router.post('/users', jwtAuth, (request, response) =>
+  createUserController.handle(request, response)
+)
+router.put('/users', jwtAuth, (request, response) => updateUserController.handle(request, response))
 router.delete('/users', (request, response) => deleteUserController.handle(request, response))
 
 // participants
-router.get('/participants', (request, response) =>
+router.get('/participants', jwtAuth, (request, response) =>
   listParticipantController.handle(request, response)
 )
-router.post('/participants', (request, response) =>
+router.post('/participants', jwtAuth, (request, response) =>
   createParticipantController.handle(request, response)
 )
-router.put('/participants', (request, response) =>
+router.put('/participants', jwtAuth, (request, response) =>
   updatePariticipantController.handle(request, response)
 )
 
