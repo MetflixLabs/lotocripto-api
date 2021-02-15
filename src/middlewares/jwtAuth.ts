259ac450c183e88ec6149168e573777bc17a851c
eber404 <bodacamargo@gmail.com>
2021-02-15 14:08:13 -0300

    fix: bypass jwt auth if bot_key is in cookies

--- src/middlewares/jwtAuth.ts
+++ src/middlewares/jwtAuth.ts
@@ -10,14 +10,20 @@ export const jwtAuth = async (
 ): Promise<Response | void> => {
   try {
     const SECRET = process.env.SECRET
+    const BOT_KEY = process.env.BOT_KEY
     const token = request.cookies.token
+    const requestBotKey = request.cookies.bot_key
 
     if (!SECRET) throw new Error('Secret não especificado nas variáveis de ambiente.')
-    if (!token) throw new Error('Token não informado.')
 
-    const decoded = jwt.verify(token, SECRET)
-
-    if (!decoded) throw new Error('Token inválido.')
+    if (BOT_KEY && requestBotKey) {
+      const isBot = requestBotKey === BOT_KEY
+      if (!isBot) throw new Error('A chave de acesso do bot está incorreta.')
+    } else {
+      if (!token) throw new Error('Token não informado.')
+      const decoded = jwt.verify(token, SECRET)
+      if (!decoded) throw new Error('Token inválido.')
+    }
 
     next()
   } catch (error) {
