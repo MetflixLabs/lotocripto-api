37e46091a0ad84eba1f6f1b03b78a63021141523
eber404 <bodacamargo@gmail.com>
2021-02-20 20:18:33 -0300

    fix: bot validation to create winner use case

--- src/useCases/Winner/CreateWinnerUseCase/CreateWinnerController.ts
+++ src/useCases/Winner/CreateWinnerUseCase/CreateWinnerController.ts
@@ -8,11 +8,26 @@ export class CreateWinnerController {
   async handle(request: Request, response: Response): Promise<Response> {
     const { amount, transactionId, userId } = request.body
 
+    const BOT_KEY = process.env.BOT_KEY
+    const requestBotKey = request.cookies.bot_key
+    const isBot = requestBotKey === BOT_KEY
+
+    if (!isBot) {
+      const outputResult = OutputResultFactory({
+        notification: {
+          success: false,
+          message: 'Sem autorização para executar esse comando'
+        }
+      })
+
+      return response.status(403).json(outputResult)
+    }
+
     try {
       const outputResult = await this.createWinnerUseCase.execute({
         amount,
         userId,
-        transactionId,
+        transactionId
       })
 
       return response.status(201).json(outputResult)
@@ -20,8 +35,8 @@ export class CreateWinnerController {
       const outputResult = OutputResultFactory({
         notification: {
           success: false,
-          message: error.message,
-        },
+          message: error.message
+        }
       })
 
       return response.status(400).json(outputResult)
