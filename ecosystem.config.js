7ce815e4d8fa0c84fd3075ccc13715225753ddb9
pt-br <ltavares@yieldstreet.com>
2021-03-10 16:58:29 -0300

    fix: update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,9 @@
 module.exports = {
   apps : [{
-    script: 'index.js',
-    watch: '.'
-  }, {
-    script: './service-worker/',
-    watch: ['./service-worker']
-  }],
-
-  deploy : {
-    production : {
-      user : 'SSH_USERNAME',
-      host : 'SSH_HOSTMACHINE',
-      ref  : 'origin/master',
-      repo : 'GIT_REPOSITORY',
-      path : 'DESTINATION_PATH',
-      'pre-deploy-local': '',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
-    }
-  }
+    name: 'api',
+    script: './src/server.ts',
+    watch: '.',
+    max_memory_restart: '450M',
+    instances: 2,
+  }]
 };
