e5df15adb4233ebd8acb7e916a65a73c23d46045
pt-br <ltavares@yieldstreet.com>
2021-03-25 11:29:39 -0300

    feat: cron restart 5am everyday

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
 module.exports = {
-  apps : [{
-    name: 'api',
-    script: 'node_modules/.bin/ts-node',
-    args: './src/server.ts',
-    watch: '.',
-    max_memory_restart: '450M',
-    instances: 2,
-  }]
-};
+  apps: [
+    {
+      name: 'api',
+      script: 'node_modules/.bin/ts-node',
+      args: './src/server.ts',
+      watch: '.',
+      max_memory_restart: '450M',
+      instances: 2,
+      cron_restart: '0 5 * * *'
+    }
+  ]
+}
